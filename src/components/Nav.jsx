b1ba48f04dc1959e6b452cb3b1162259f70417a9
Areeba Fatema <areebafatema9058@gmail.com>
2025-02-17 17:00:10 +0530

    add code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/Logo.png";
 import '../css/Nav.css'
 const Nav = () => {
   return (
